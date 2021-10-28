b2f5d500d067e75de128e989eb8c3024418cdb83
EC2 Default User <ec2-user@ip-172-31-53-179.ec2.internal>
2021-10-28 13:09:18 +0000

    Update

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
+ before_action :authenticate_user!
 
   def create
     @book = Book.find(params[:book_id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
 
   has_many :books
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   attachment :profile_image
 
 end
