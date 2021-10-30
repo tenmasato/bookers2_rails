546e23a334e8c0a23552ac62f5d74b9923c85000
tenmasato <narito1015@icloud.com>
2021-10-30 12:21:33 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
     resource :favorites,only:[:create,:destroy]
     resources :book_comments,only:[:create,:destroy]
   end
-  resources :users,only:[:show,:new,:index,:edit,:create,:update]
+  resources :users,only:[:show,:new,:index,:edit,:create,:update]do
+    resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
